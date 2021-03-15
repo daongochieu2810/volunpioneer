24514702eb6127777a58fd3f91c9f377bf5a661c
daongochieu2810 <daongochieu2810@yahoo.com.vn>
2021-03-16 02:28:32 +0800

    Update org page

--- src/components/Organiser/Organiser.js
+++ src/components/Organiser/Organiser.js
@@ -1,37 +1,36 @@
 import React, { useState } from "react";
 import { connect } from "react-redux";
-import ButtonBase from "@material-ui/core/ButtonBase";
-import Modal from "@material-ui/core/Modal";
+import { Grid, ButtonBase } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import FormControl from "@material-ui/core/FormControl";
-import ListItemText from "@material-ui/core/ListItemText";
 import Select from "@material-ui/core/Select";
 import Chip from "@material-ui/core/Chip";
 import InputLabel from "@material-ui/core/InputLabel";
 import { strengthQuizQuestions } from "../Quiz/QuizData";
 import MenuItem from "@material-ui/core/MenuItem";
 import Input from "@material-ui/core/Input";
-
+import Lottie from "lottie-react";
+import FormAnim from "../../images/form.json";
+import Done from "../../images/done.json";
 import { TextField } from "@material-ui/core";
+import "./Org.css";
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   formControl: {
     margin: theme.spacing(1),
-    minWidth: 120,
-    maxWidth: 300
+    minWidth: 200,
+    maxWidth: 500,
+    padding: 20,
   },
   chips: {
     display: "flex",
-    flexWrap: "wrap"
+    flexWrap: "wrap",
   },
   chip: {
-    margin: 2
+    margin: 2,
   },
   noLabel: {
-    marginTop: theme.spacing(3)
-  },
-  col: {
-    display: "block"
+    marginTop: theme.spacing(3),
   },
   paper: {
     position: "absolute",
@@ -40,8 +39,8 @@ const useStyles = makeStyles(theme => ({
     border: "1px solid #000",
     boxShadow: theme.shadows[5],
     padding: theme.spacing(2, 4, 3),
-    zIndex: 10
-  }
+    zIndex: 10,
+  },
 }));
 
 const getModalStyle = () => {
@@ -51,12 +50,12 @@ const getModalStyle = () => {
   return {
     top: `${top}%`,
     left: `${left}%`,
-    transform: `translate(-${top}%, -${left}%)`
+    transform: `translate(-${top}%, -${left}%)`,
   };
 };
 
 const Organiser = ({ uploadVolunteerActivity }) => {
-  const [isUploading, setIsUploading] = useState(false);
+  const [isUploaded, setIsUploaded] = useState(false);
   const classes = useStyles();
   const modalStyle = getModalStyle();
   const ITEM_HEIGHT = 48;
@@ -65,24 +64,23 @@ const Organiser = ({ uploadVolunteerActivity }) => {
     PaperProps: {
       style: {
         maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-        width: 250
-      }
-    }
+        width: 250,
+      },
+    },
   };
   const [formState, setFormState] = useState({
     Title: null,
-    Description: null
+    Description: null,
   });
   const [selectedStrengths, setSelectedStrengths] = useState([]);
-  const handleChange = event => {
+  const handleChange = (event) => {
     const str = event.target.value;
     setSelectedStrengths(str);
   };
   const allStrengths = [];
-  Object.keys(strengthQuizQuestions).forEach(key =>
-    strengthQuizQuestions[key].forEach(str => allStrengths.push(str))
+  Object.keys(strengthQuizQuestions).forEach((key) =>
+    strengthQuizQuestions[key].forEach((str) => allStrengths.push(str))
   );
-  console.log(allStrengths);
 
   const updateFormStateHandler = (field, event) => {
     const updatedFormState = { [field]: event.target.value };
@@ -92,100 +90,191 @@ const Organiser = ({ uploadVolunteerActivity }) => {
   const uploadHandler = () => {
     const volunteerActivity = { ...formState, strengths: selectedStrengths };
     uploadVolunteerActivity(volunteerActivity);
+    setIsUploaded(true);
   };
 
-  return (
-    <div>
-      <ButtonBase
-        focusRipple
+  return isUploaded ? (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: 'column',
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <div
         style={{
-          marginTop: 30,
-          borderRadius: 10,
-          backgroundColor: "black",
-          paddingTop: 20,
-          paddingBottom: 20,
-          color: "white",
-          paddingLeft: 50,
-          paddingRight: 50,
-          fontSize: 20
+          width: 400,
         }}
-        onClick={() => setIsUploading(true)}
       >
-        Upload Volunteer Activity!
-      </ButtonBase>
-      <div hidden={!isUploading}>
-        <form>
-          <TextField
-            className={classes.col}
-            name='Title'
-            label='Title'
-            variant='outlined'
-            onChange={event => updateFormStateHandler("Title", event)}
-            style={{ width: "32%" }}
-          />
-          <TextField
-            className={classes.col}
-            name='Description'
-            label='Description'
-            variant='outlined'
-            onChange={event => updateFormStateHandler("Description", event)}
-            style={{ width: "32%" }}
-          />
-          <FormControl className={[classes.formControl, classes.col].join(" ")}>
-            <InputLabel id='demo-mutiple-chip-label'>Strengths</InputLabel>
-            <Select
-              labelId='demo-mutiple-chip-label'
-              id='demo-mutiple-chip'
-              multiple
-              value={selectedStrengths}
-              onChange={handleChange}
-              input={<Input id='select-multiple-chip' />}
-              renderValue={selected => (
-                <div className={classes.chips}>
-                  {selected.map(value => (
-                    <Chip key={value} label={value} className={classes.chip} />
-                  ))}
-                </div>
-              )}
-              MenuProps={MenuProps}
-            >
-              {allStrengths.map(str => (
-                <MenuItem key={str} value={str}>
-                  {str}
-                </MenuItem>
-              ))}
-            </Select>
-          </FormControl>
-          <ButtonBase
-            focusRipple
-            style={{
-              marginTop: 30,
-              borderRadius: 10,
-              backgroundColor: "black",
-              paddingTop: 20,
-              paddingBottom: 20,
-              color: "white",
-              paddingLeft: 50,
-              paddingRight: 50,
-              fontSize: 20
-            }}
-            onClick={() => uploadHandler()}
-          >
-            Upload
-          </ButtonBase>
-        </form>
+        <Lottie animationData={Done} />
       </div>
+      <p
+        style={{
+          textAlign: "center",
+          fontFamily: "Roboto",
+          fontSize: "30px",
+          fontStyle: "normal",
+          fontWeight: "bold",
+          lineHeight: "30px",
+          marginBottom: 50,
+        }}
+      >
+        You're set !
+      </p>
+      <p
+        style={{
+          textAlign: "center",
+          fontFamily: "Roboto",
+          fontSize: "25px",
+          fontStyle: "normal",
+          fontWeight: "normal",
+          lineHeight: "30px",
+          marginBottom: 50,
+        }}
+      >
+        Now wait for your best candidates to apply !
+      </p>
+    </div>
+  ) : (
+    <div>
+      <Grid container>
+        <Grid item xs={6}>
+          <Lottie animationData={FormAnim} />
+        </Grid>
+        <Grid
+          item
+          xs={6}
+          style={{
+            backgroundColor: "black",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <form
+            style={{
+              backgroundColor: "white",
+              width: "70%",
+              height: "80%",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+              borderRadius: 10,
+            }}
+          >
+            <p
+              style={{
+                textAlign: "center",
+                fontFamily: "Roboto",
+                fontSize: "30px",
+                fontStyle: "normal",
+                fontWeight: "bold",
+                lineHeight: "30px",
+                marginBottom: 50,
+              }}
+            >
+              Find your best volunteers now !
+            </p>
+            <TextField
+              name="Title"
+              label="Title"
+              variant="outlined"
+              onChange={(event) => updateFormStateHandler("Title", event)}
+              style={{
+                width: "70%",
+                marginBottom: 30,
+                borderRadius: 10,
+              }}
+            />
+            <TextField
+              name="Description"
+              label="Description"
+              variant="outlined"
+              onChange={(event) => updateFormStateHandler("Description", event)}
+              style={{
+                width: "70%",
+
+                borderRadius: 10,
+                marginBottom: 20,
+              }}
+            />
+            <FormControl
+              className={[classes.formControl].join(" ")}
+              style={{
+                borderRadius: 5,
+              }}
+            >
+              <InputLabel
+                id="demo-mutiple-chip-label"
+                style={{
+                  padding: 10,
+                  margin: 10,
+                }}
+              >
+                Strengths
+              </InputLabel>
+              <Select
+                labelId="demo-mutiple-chip-label"
+                id="demo-mutiple-chip"
+                multiple
+                value={selectedStrengths}
+                onChange={handleChange}
+                input={<Input id="select-multiple-chip" />}
+                renderValue={(selected) => (
+                  <div className={classes.chips}>
+                    {selected.map((value) => (
+                      <Chip
+                        key={value}
+                        label={value}
+                        className={classes.chip}
+                      />
+                    ))}
+                  </div>
+                )}
+                MenuProps={MenuProps}
+              >
+                {allStrengths.map((str) => (
+                  <MenuItem key={str} value={str}>
+                    {str}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+            <ButtonBase
+              focusRipple
+              className="hvr-rotate"
+              style={{
+                marginTop: 30,
+                borderRadius: 10,
+                backgroundColor: "black",
+                paddingTop: 20,
+                paddingBottom: 20,
+                color: "white",
+                paddingLeft: 50,
+                paddingRight: 50,
+                fontSize: 20,
+              }}
+              onClick={() => uploadHandler()}
+            >
+              Upload
+            </ButtonBase>
+          </form>
+        </Grid>
+      </Grid>
     </div>
   );
 };
 
-const mapDispatchToProps = dispatch => {
+const mapDispatchToProps = (dispatch) => {
   return {
-    uploadVolunteerActivity: activity =>
+    uploadVolunteerActivity: (activity) =>
       dispatch({
         type: "UPLOAD_VOLUNTEER_ACTIVITY",
-        volunteerActivity: activity
-      })
+        volunteerActivity: activity,
+      }),
   };
 };
 
