5a78fc9d18f2e8a5d3a7359b0c1ae4ae314e7d30
olivia johansen <oliviajjohansen@gmail.com>
2021-03-16 01:32:30 +0800

    Add organiser upload and volunteer my activities page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Quiz from "./components/Quiz/Quiz";
+import Organiser from "./components/Organiser/Organiser";
+import Volunteer from "./components/Volunteer/Volunteer";
 
 function App() {
   return (
     <div className='App'>
-      <Quiz />
+      <Volunteer />
     </div>
   );
 }
